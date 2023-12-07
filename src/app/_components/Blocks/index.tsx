4c84b853999f853882cf411a68bf7ecb4a81b1d5
Davide Filosa <davideclaypool@gmail.com!>
2023-12-07 15:53:26 +0100

    fixed types error

--- src/app/_components/Blocks/index.tsx
+++ src/app/_components/Blocks/index.tsx
@@ -24,6 +24,7 @@ const blockComponents = {
 export const Blocks: React.FC<{
   blocks: (Page['layout'][0] | RelatedProductsProps)[]
   disableTopPadding?: boolean
+  disableBottomPadding?: boolean
 }> = props => {
   const { disableTopPadding, blocks } = props
 
